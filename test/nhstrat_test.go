75e0777200ece2d00d81268eaf02b81f32d41d69
deroshkin <deroshkinsf@gmail.com>
2022-08-17 19:48:36 -0400

    Test Refactoring Part 2
    Changed testing for candidate changes using util.CandTester

--- test/nhstrat_test.go
+++ test/nhstrat_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/deroshkin/sudoku/pkg/nhstrats"
 	"github.com/deroshkin/sudoku/pkg/solver"
 	"github.com/deroshkin/sudoku/util"
-	"golang.org/x/exp/slices"
 )
 
 func TestHiddenSingles(t *testing.T) {
@@ -19,9 +18,13 @@ func TestHiddenSingles(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol1 := solver.MakeSolver(board1, []solver.Strategy{})
-	if nhstrats.HiddenSingles(sol1.Cands) {
-		t.Fatalf("Board %v should not have any hidden singles", board1)
+	res1, msg1 := util.CandTester(board1, nhstrats.HiddenSingles, false,
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res1 {
+		t.Fatalf(msg1)
 	}
 
 	board2 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -33,16 +36,13 @@ func TestHiddenSingles(t *testing.T) {
 		{0, 0, 1, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol2 := solver.MakeSolver(board2, []solver.Strategy{})
-	sol2.RestrictCands()
-	if !nhstrats.HiddenSingles(sol2.Cands) {
-		t.Fatalf("Board %v should have a hidden single 1 in row 1 (r1c2)", board2)
-	}
-	if len(sol2.Cands[0][1]) != 1 {
-		t.Fatalf("Hidden single in r1c2 not restricted in %v, still %v candidates", board2, len(sol2.Cands[0][1]))
-	}
-	if sol2.Cands[0][1][0] != 1 {
-		t.Fatalf("Incorrect hidden single in r1c2 of %v, expected 1, got %v", board2, sol2.Cands[0][1][0])
+	res2, msg2 := util.CandTester(board2, nhstrats.HiddenSingles, true,
+		map[util.Cell][]uint8{{R: 0, C: 1}: {1}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res2 {
+		t.Fatalf(msg2)
 	}
 
 	board3 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -54,16 +54,13 @@ func TestHiddenSingles(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 2, 0, 0, 0}}
-	sol3 := solver.MakeSolver(board3, []solver.Strategy{})
-	sol3.RestrictCands()
-	if !nhstrats.HiddenSingles(sol3.Cands) {
-		t.Fatalf("Board %v should have a hidden single 2 in box 5 (r4c5)", board3)
-	}
-	if len(sol3.Cands[3][4]) != 1 {
-		t.Fatalf("Hidden single in r4c5 not restricted in %v, still %v candidates", board3, len(sol3.Cands[3][4]))
-	}
-	if sol3.Cands[3][4][0] != 2 {
-		t.Fatalf("Incorrect hidden single in r4c5 of %v, expected 2, got %v", board3, sol3.Cands[3][4][0])
+	res3, msg3 := util.CandTester(board3, nhstrats.HiddenSingles, true,
+		map[util.Cell][]uint8{{R: 3, C: 4}: {2}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res3 {
+		t.Fatalf(msg3)
 	}
 
 	board4 := [][]uint8{{0, 0, 0, 0, 0, 0, 7, 0, 0},
@@ -75,21 +72,18 @@ func TestHiddenSingles(t *testing.T) {
 		{0, 0, 0, 0, 7, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{7, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol4 := solver.MakeSolver(board4, []solver.Strategy{})
-	sol4.RestrictCands()
-	if !nhstrats.HiddenSingles(sol4.Cands) {
-		t.Fatalf("Board %v should have a hidden single 7 in column 8 (r8c8)", board4)
-	}
-	if len(sol4.Cands[7][7]) != 1 {
-		t.Fatalf("Hidden single in r8c8 not restricted in %v, still %v candidates", board4, len(sol4.Cands[7][7]))
-	}
-	if sol4.Cands[7][7][0] != 7 {
-		t.Fatalf("Incorrect hidden single in r8c8 of %v, expected 7, got %v", board4, sol4.Cands[7][7][0])
+	res4, msg4 := util.CandTester(board4, nhstrats.HiddenSingles, true,
+		map[util.Cell][]uint8{{R: 7, C: 7}: {7}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res4 {
+		t.Fatalf(msg4)
 	}
 }
 
 func TestHiddenSinglesSolve(t *testing.T) {
-	board1 := [][]uint8{{0, 0, 0, 0, 0, 4, 0, 2, 8},
+	board := [][]uint8{{0, 0, 0, 0, 0, 4, 0, 2, 8},
 		{4, 0, 6, 0, 0, 0, 0, 0, 5},
 		{1, 0, 0, 0, 3, 0, 6, 0, 0},
 		{0, 0, 0, 3, 0, 1, 0, 0, 0},
@@ -98,11 +92,13 @@ func TestHiddenSinglesSolve(t *testing.T) {
 		{0, 0, 2, 0, 1, 0, 0, 0, 3},
 		{9, 0, 0, 0, 0, 0, 5, 0, 7},
 		{6, 7, 0, 4, 0, 0, 0, 0, 0}}
-	res1, msg1 := util.SolveTester(board1, [][]uint8{}, []solver.Strategy{}, false)
+	res1, msg1 := util.SolveTester(board, [][]uint8{}, []solver.Strategy{},
+		false,
+	)
 	if !res1 {
 		t.Fatalf(msg1)
 	}
-	board1Solved := [][]uint8{{7, 3, 5, 1, 6, 4, 9, 2, 8},
+	boardSolved := [][]uint8{{7, 3, 5, 1, 6, 4, 9, 2, 8},
 		{4, 2, 6, 9, 7, 8, 3, 1, 5},
 		{1, 9, 8, 5, 3, 2, 6, 7, 4},
 		{2, 4, 9, 3, 8, 1, 7, 5, 6},
@@ -111,7 +107,9 @@ func TestHiddenSinglesSolve(t *testing.T) {
 		{8, 5, 2, 6, 1, 7, 4, 9, 3},
 		{9, 1, 4, 8, 2, 3, 5, 6, 7},
 		{6, 7, 3, 4, 9, 5, 2, 8, 1}}
-	res2, msg2 := util.SolveTester(board1, board1Solved, []solver.Strategy{nhstrats.HiddenSingles}, true)
+	res2, msg2 := util.SolveTester(board, boardSolved,
+		[]solver.Strategy{nhstrats.HiddenSingles}, true,
+	)
 	if !res2 {
 		t.Fatalf(msg2)
 	}
@@ -127,21 +125,15 @@ func TestNakedPairs(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol1 := solver.MakeSolver(board1, []solver.Strategy{})
-	sol1.RestrictCands()
-	if !nhstrats.NakedPairs(sol1.Cands) {
-		t.Fatalf("Board %v has a naked pair (12) in row 3, but it was not detected", board1)
-	}
-	if !slices.Equal(sol1.Cands[2][1], []uint8{1, 2}) {
-		t.Fatalf("Expected to have candidates 1 & 2 in r3c2, got %v instead", sol1.Cands[2][1])
-	}
-	if !slices.Equal(sol1.Cands[2][2], []uint8{1, 2}) {
-		t.Fatalf("Expected to have candidates 1 & 2 in r3c3, got %v instead", sol1.Cands[2][1])
-	}
-	for i := 3; i < 9; i++ {
-		if slices.Contains(sol1.Cands[2][i], 1) || slices.Contains(sol1.Cands[2][i], 2) {
-			t.Fatalf("Naked pair candidates in r3c%v not removed, have %v", i+1, sol1.Cands[2][i])
-		}
+	res1, msg1 := util.CandTester(board1, nhstrats.NakedPairs, true,
+		map[util.Cell][]uint8{{R: 2, C: 1}: {1, 2}, {R: 2, C: 2}: {1, 2}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 2, C: 0}: {1, 2}, {R: 2, C: 3}: {1, 2},
+			{R: 2, C: 4}: {1, 2}, {R: 2, C: 5}: {1, 2}, {R: 2, C: 6}: {1, 2},
+			{R: 2, C: 7}: {1, 2}, {R: 2, C: 8}: {1, 2}},
+	)
+	if !res1 {
+		t.Fatalf(msg1)
 	}
 
 	board2 := [][]uint8{{5, 3, 2, 1, 0, 7, 9, 6, 0},
@@ -153,21 +145,15 @@ func TestNakedPairs(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol2 := solver.MakeSolver(board2, []solver.Strategy{})
-	sol2.RestrictCands()
-	if !nhstrats.NakedPairs(sol2.Cands) {
-		t.Fatalf("Board %v has a naked pair (48) in column 5, but it was not detected", board2)
-	}
-	if !slices.Equal(sol2.Cands[0][4], []uint8{4, 8}) {
-		t.Fatalf("Expected to have candidates 4 & 8 in r1c5, got %v instead", sol2.Cands[0][4])
-	}
-	if !slices.Equal(sol2.Cands[3][4], []uint8{4, 8}) {
-		t.Fatalf("Expected to have candidates 4 & 8 in r4c5, got %v instead", sol2.Cands[3][4])
-	}
-	for i := 0; i < 9; i++ {
-		if i != 0 && i != 3 && (slices.Contains(sol2.Cands[i][4], 4) || slices.Contains(sol2.Cands[i][4], 8)) {
-			t.Fatalf("Naked pair candidates in r%vc4 not removed, have %v", i+1, sol2.Cands[2][i])
-		}
+	res2, msg2 := util.CandTester(board2, nhstrats.NakedPairs, true,
+		map[util.Cell][]uint8{{R: 0, C: 4}: {4, 8}, {R: 3, C: 4}: {4, 8}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 1, C: 4}: {4, 8}, {R: 2, C: 4}: {4, 8},
+			{R: 4, C: 4}: {4, 8}, {R: 5, C: 4}: {4, 8}, {R: 6, C: 4}: {4, 8},
+			{R: 7, C: 4}: {4, 8}, {R: 8, C: 4}: {4, 8}},
+	)
+	if !res2 {
+		t.Fatalf(msg2)
 	}
 
 	board3 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -179,23 +165,15 @@ func TestNakedPairs(t *testing.T) {
 		{1, 2, 3, 4, 5, 6, 7, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol3 := solver.MakeSolver(board3, []solver.Strategy{})
-	sol3.RestrictCands()
-	if !nhstrats.NakedPairs(sol3.Cands) {
-		t.Fatalf("Board %v has a naked pair (89) in box 9, but it was not detected", board3)
-	}
-	if !slices.Equal(sol3.Cands[6][7], []uint8{8, 9}) {
-		t.Fatalf("Expected to have candidates 8 & 9 in r7c8, got %v instead", sol3.Cands[6][7])
-	}
-	if !slices.Equal(sol3.Cands[6][8], []uint8{8, 9}) {
-		t.Fatalf("Expected to have candidates 8 & 9 in r7c9, got %v instead", sol3.Cands[6][8])
-	}
-	for i := 7; i < 9; i++ {
-		for j := 6; j < 9; j++ {
-			if slices.Contains(sol3.Cands[i][j], 8) || slices.Contains(sol3.Cands[i][j], 9) {
-				t.Fatalf("Naked pair candidates in r%vc%v not removed, have %v", i+1, j+1, sol3.Cands[2][i])
-			}
-		}
+	res3, msg3 := util.CandTester(board3, nhstrats.NakedPairs, true,
+		map[util.Cell][]uint8{{R: 6, C: 7}: {8, 9}, {R: 6, C: 8}: {8, 9}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 6, C: 6}: {8, 9}, {R: 7, C: 6}: {8, 9},
+			{R: 7, C: 7}: {8, 9}, {R: 7, C: 8}: {8, 9}, {R: 8, C: 6}: {8, 9},
+			{R: 8, C: 7}: {8, 9}, {R: 8, C: 8}: {8, 9}},
+	)
+	if !res3 {
+		t.Fatalf(msg3)
 	}
 
 	board4 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -207,10 +185,13 @@ func TestNakedPairs(t *testing.T) {
 		{1, 2, 0, 4, 5, 6, 7, 0, 9},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol4 := solver.MakeSolver(board4, []solver.Strategy{})
-	sol4.RestrictCands()
-	if nhstrats.NakedPairs(sol4.Cands) {
-		t.Fatalf("Board %v should not have naked pairs, but the program somehow found 1", board4)
+	res4, msg4 := util.CandTester(board4, nhstrats.NakedPairs, false,
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res4 {
+		t.Fatalf(msg4)
 	}
 }
 
@@ -224,24 +205,16 @@ func TestNakedTriples(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol1 := solver.MakeSolver(board1, []solver.Strategy{})
-	sol1.RestrictCands()
-	if !nhstrats.NakedTriples(sol1.Cands) {
-		t.Fatalf("Board %v should have a naked triple (129) in row 3, but none found", board1)
-	}
-	if !slices.Equal(sol1.Cands[2][0], []uint8{1, 2, 9}) {
-		t.Fatalf("Expected to have candidates 1, 2 & 9 in r3c1, got %v instead", sol1.Cands[2][1])
-	}
-	if !slices.Equal(sol1.Cands[2][1], []uint8{1, 2, 9}) {
-		t.Fatalf("Expected to have candidates 1, 2 & 9 in r3c2, got %v instead", sol1.Cands[2][1])
-	}
-	if !slices.Equal(sol1.Cands[2][2], []uint8{1, 2, 9}) {
-		t.Fatalf("Expected to have candidates 1, 2 &9 in r3c3, got %v instead", sol1.Cands[2][1])
-	}
-	for i := 3; i < 9; i++ {
-		if slices.Contains(sol1.Cands[2][i], 1) || slices.Contains(sol1.Cands[2][i], 2) || slices.Contains(sol1.Cands[2][i], 9) {
-			t.Fatalf("Naked triple candidates in r3c%v not removed, have %v", i+1, sol1.Cands[2][i])
-		}
+	res1, msg1 := util.CandTester(board1, nhstrats.NakedTriples, true,
+		map[util.Cell][]uint8{{R: 2, C: 0}: {1, 2, 9}, {R: 2, C: 1}: {1, 2, 9},
+			{R: 2, C: 2}: {1, 2, 9}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 2, C: 3}: {1, 2, 9}, {R: 2, C: 4}: {1, 2, 9},
+			{R: 2, C: 5}: {1, 2, 9}, {R: 2, C: 6}: {1, 2, 9},
+			{R: 2, C: 7}: {1, 2, 9}, {R: 2, C: 8}: {1, 2, 9}},
+	)
+	if !res1 {
+		t.Fatalf(msg1)
 	}
 
 	board2 := [][]uint8{{5, 3, 2, 1, 0, 7, 9, 0, 0},
@@ -253,29 +226,16 @@ func TestNakedTriples(t *testing.T) {
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol2 := solver.MakeSolver(board2, []solver.Strategy{})
-	sol2.RestrictCands()
-	if !nhstrats.NakedTriples(sol2.Cands) {
-		t.Fatalf("Board %v should have a naked triple (468) in column 5, but none found", board2)
-	}
-	if !slices.Equal(sol2.Cands[0][4], []uint8{4, 6, 8}) {
-		t.Fatalf("Expected to have candidates 4, 6 & 8 in r1c5, got %v instead", sol2.Cands[0][4])
-	}
-	if !slices.Equal(sol2.Cands[3][4], []uint8{4, 6, 8}) {
-		t.Fatalf("Expected to have candidates 4, 6 & 8 in r4c5, got %v instead", sol2.Cands[3][4])
-	}
-	if !slices.Equal(sol2.Cands[5][4], []uint8{4, 6, 8}) {
-		t.Fatalf("Expected to have candidates 4, 6 & 8 in r6c5, got %v instead", sol2.Cands[5][4])
-	}
-	for i := 1; i < 3; i++ {
-		if slices.Contains(sol2.Cands[i][4], 4) || slices.Contains(sol2.Cands[i][4], 6) || slices.Contains(sol2.Cands[i][4], 8) {
-			t.Fatalf("Naked triple candidates in r%vc5 not removed, have %v", i+1, sol2.Cands[i][4])
-		}
-	}
-	for i := 6; i < 9; i++ {
-		if slices.Contains(sol2.Cands[i][4], 4) || slices.Contains(sol2.Cands[i][4], 6) || slices.Contains(sol2.Cands[i][4], 8) {
-			t.Fatalf("Naked triple candidates in r%vc5 not removed, have %v", i+1, sol2.Cands[i][4])
-		}
+	res2, msg2 := util.CandTester(board2, nhstrats.NakedTriples, true,
+		map[util.Cell][]uint8{{R: 0, C: 4}: {4, 6, 8}, {R: 3, C: 4}: {4, 6, 8},
+			{R: 5, C: 4}: {4, 6, 8}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 1, C: 4}: {4, 6, 8}, {R: 2, C: 4}: {4, 6, 8},
+			{R: 4, C: 4}: {4, 6, 8}, {R: 6, C: 4}: {4, 6, 8},
+			{R: 7, C: 4}: {4, 6, 8}, {R: 8, C: 4}: {4, 6, 8}},
+	)
+	if !res2 {
+		t.Fatalf(msg2)
 	}
 
 	board3 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -287,26 +247,16 @@ func TestNakedTriples(t *testing.T) {
 		{1, 2, 3, 4, 5, 6, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol3 := solver.MakeSolver(board3, []solver.Strategy{})
-	sol3.RestrictCands()
-	if !nhstrats.NakedTriples(sol3.Cands) {
-		t.Fatalf("Board %v has a naked triple (789) in box 9, but it was not detected", board3)
-	}
-	if !slices.Equal(sol3.Cands[6][6], []uint8{7, 8, 9}) {
-		t.Fatalf("Expected to have candidates 7, 8 & 9 in r7c8, got %v instead", sol3.Cands[6][7])
-	}
-	if !slices.Equal(sol3.Cands[6][7], []uint8{7, 8, 9}) {
-		t.Fatalf("Expected to have candidates 7, 8 & 9 in r7c8, got %v instead", sol3.Cands[6][7])
-	}
-	if !slices.Equal(sol3.Cands[6][8], []uint8{7, 8, 9}) {
-		t.Fatalf("Expected to have candidates 7, 8 & 9 in r7c9, got %v instead", sol3.Cands[6][8])
-	}
-	for i := 7; i < 9; i++ {
-		for j := 6; j < 9; j++ {
-			if slices.Contains(sol3.Cands[i][j], 7) || slices.Contains(sol3.Cands[i][j], 8) || slices.Contains(sol3.Cands[i][j], 9) {
-				t.Fatalf("Naked triple candidates in r%vc%v not removed, have %v", i+1, j+1, sol3.Cands[2][i])
-			}
-		}
+	res3, msg3 := util.CandTester(board3, nhstrats.NakedTriples, true,
+		map[util.Cell][]uint8{{R: 6, C: 6}: {7, 8, 9}, {R: 6, C: 7}: {7, 8, 9},
+			{R: 6, C: 8}: {7, 8, 9}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 7, C: 6}: {7, 8, 9}, {R: 7, C: 7}: {7, 8, 9},
+			{R: 7, C: 8}: {7, 8, 9}, {R: 8, C: 6}: {7, 8, 9},
+			{R: 8, C: 7}: {7, 8, 9}, {R: 8, C: 8}: {7, 8, 9}},
+	)
+	if !res3 {
+		t.Fatalf(msg3)
 	}
 
 	board4 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 8, 0},
@@ -318,26 +268,16 @@ func TestNakedTriples(t *testing.T) {
 		{1, 2, 3, 4, 5, 6, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol4 := solver.MakeSolver(board4, []solver.Strategy{})
-	sol4.RestrictCands()
-	if !nhstrats.NakedTriples(sol4.Cands) {
-		t.Fatalf("Board %v has a naked triple (789) in box 9, but it was not detected", board4)
-	}
-	if !slices.Equal(sol4.Cands[6][6], []uint8{8, 9}) {
-		t.Fatalf("Expected to have candidates 8 & 9 in r7c8, got %v instead", sol4.Cands[6][7])
-	}
-	if !slices.Equal(sol4.Cands[6][7], []uint8{7, 9}) {
-		t.Fatalf("Expected to have candidates 7 & 9 in r7c8, got %v instead", sol4.Cands[6][7])
-	}
-	if !slices.Equal(sol4.Cands[6][8], []uint8{7, 8}) {
-		t.Fatalf("Expected to have candidates 7 & 8 in r7c9, got %v instead", sol4.Cands[6][8])
-	}
-	for i := 7; i < 9; i++ {
-		for j := 6; j < 9; j++ {
-			if slices.Contains(sol4.Cands[i][j], 7) || slices.Contains(sol4.Cands[i][j], 8) || slices.Contains(sol4.Cands[i][j], 9) {
-				t.Fatalf("Naked triple candidates in r%vc%v not removed, have %v", i+1, j+1, sol4.Cands[2][i])
-			}
-		}
+	res4, msg4 := util.CandTester(board4, nhstrats.NakedTriples, true,
+		map[util.Cell][]uint8{{R: 6, C: 6}: {8, 9}, {R: 6, C: 7}: {7, 9},
+			{R: 6, C: 8}: {7, 8}},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{{R: 7, C: 6}: {7, 8, 9}, {R: 7, C: 7}: {7, 8, 9},
+			{R: 7, C: 8}: {7, 8, 9}, {R: 8, C: 6}: {7, 8, 9},
+			{R: 8, C: 7}: {7, 8, 9}, {R: 8, C: 8}: {7, 8, 9}},
+	)
+	if !res4 {
+		t.Fatalf(msg4)
 	}
 
 	board5 := [][]uint8{{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -349,10 +289,13 @@ func TestNakedTriples(t *testing.T) {
 		{1, 2, 0, 4, 5, 6, 0, 0, 9},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0}}
-	sol5 := solver.MakeSolver(board5, []solver.Strategy{})
-	sol5.RestrictCands()
-	if nhstrats.NakedTriples(sol5.Cands) {
-		t.Fatalf("Board %v should have no naked triples, but one was detected", board5)
+	res5, msg5 := util.CandTester(board5, nhstrats.NakedTriples, false,
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+		map[util.Cell][]uint8{},
+	)
+	if !res5 {
+		t.Fatalf(msg5)
 	}
 }
 
@@ -375,7 +318,11 @@ func TestNakedPairsTriplesSolve(t *testing.T) {
 		{7, 4, 9, 1, 6, 8, 2, 3, 5},
 		{6, 1, 2, 4, 3, 5, 9, 8, 7},
 		{8, 5, 3, 7, 2, 9, 6, 4, 1}}
-	res, msg := util.SolveTester(board, boardSolved, []solver.Strategy{nhstrats.HiddenSingles, nhstrats.NakedPairs, nhstrats.NakedTriples}, true)
+	res, msg := util.SolveTester(board, boardSolved,
+		[]solver.Strategy{nhstrats.HiddenSingles, nhstrats.NakedPairs,
+			nhstrats.NakedTriples},
+		true,
+	)
 	if !res {
 		t.Fatalf(msg)
 	}
--- util/test_util.go
+++ util/test_util.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 
 	"github.com/deroshkin/sudoku/pkg/solver"
+	"golang.org/x/exp/slices"
 )
+
+type Cell struct {
+	R, C uint8
+}
 
 func SolveTester(board, answer [][]uint8, strats []solver.Strategy, solvable bool) (bool, string) {
 	sol := solver.MakeSolver(board, strats)
@@ -23,5 +28,42 @@ func SolveTester(board, answer [][]uint8, strats []solver.Strategy, solvable boo
 			}
 		}
 	}
-	return true, ""
+	return true, "Test successful"
+}
+
+func CandTester(board [][]uint8, strat solver.Strategy, expected bool,
+	equal map[Cell][]uint8, contains map[Cell][]uint8, dnc map[Cell][]uint8) (bool, string) {
+	sol := solver.MakeSolver(board, []solver.Strategy{})
+	sol.RestrictCands()
+
+	change := strat(sol.Cands)
+	if !expected && change {
+		return false, fmt.Sprintf("Strategy %v made changes when none were expected", strat)
+	} else if expected && !change {
+		return false, fmt.Sprintf("Strategy %v made no changes when some were expected", strat)
+	} else if expected {
+		for cell, vals := range equal {
+			if !slices.Equal(sol.Cands[cell.R][cell.C], vals) {
+				return false, fmt.Sprintf("Expected r%vc%v to have candidates %v, but got %v instead",
+					cell.R+1, cell.C+1, vals, sol.Cands[cell.R][cell.C])
+			}
+		}
+		for cell, vals := range contains {
+			for _, v := range vals {
+				if !slices.Contains(sol.Cands[cell.R][cell.C], v) {
+					return false, fmt.Sprintf("Expected r%vc%v to contain candidates %v, but got %v instead",
+						cell.R+1, cell.C+1, vals, sol.Cands[cell.R][cell.C])
+				}
+			}
+		}
+		for cell, vals := range dnc {
+			for _, v := range vals {
+				if slices.Contains(sol.Cands[cell.R][cell.C], v) {
+					return false, fmt.Sprintf("Expected candidates %v to be missing from r%vc%v, but have candidates %v",
+						vals, cell.R+1, cell.C+1, sol.Cands[cell.R][cell.C])
+				}
+			}
+		}
+	}
+	return true, "Test successful"
 }
