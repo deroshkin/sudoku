38a3a77da00d95a97c3877ed83bf1d70f51ec1ea
deroshkin <deroshkinsf@gmail.com>
2022-08-21 08:38:50 -0400

    Added doc strings to test_util.go

--- util/test_util.go
+++ util/test_util.go
@@ -1,3 +1,5 @@
+// Package util provides supporting functions for other packages and tests
+// that are not meant to be invoked directly otherwise.
 package util
 
 import (
@@ -7,10 +9,22 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+// Cell is a simple tuple to encode cell coordinates.
 type Cell struct {
 	R, C uint8
 }
 
+// SolveTester is a utility function for tests that checks whether a given sudoku board is solvable
+// using the provided strategies, and if it is solvable, that the answer matches the provided one.
+// As input it takes the initial board, the answer (can be [][]uint8{} if the board should not be solvable),
+// a list of strategies to be used and a boolean to idicate whether the board should be solvable.
+// The return is either (true, "") if the test passed, or (false, error_message) if the test failed.
+// The expected use is:
+// res, msg = SolveTester(...)
+//
+//	if !res {
+//	    t.Fatalf(msg)
+//	}
 func SolveTester(board, answer [][]uint8, strats []solver.Strategy, solvable bool) (bool, string) {
 	sol := solver.MakeSolver(board, strats)
 	solved := sol.Solve()
@@ -31,6 +45,19 @@ func SolveTester(board, answer [][]uint8, strats []solver.Strategy, solvable boo
 	return true, "Test successful"
 }
 
+// CandTester is a utility function for tests to check whether the given strategy works correctly.
+// As input, this function takes a starting board, a strategy, expected outcome of the strategy (change/no change),
+// and three maps Cell -> []uint8:
+// equal for when the values in the indicated cell must equal the provided slice
+// contains for when the values in the indicated cell must contain the provided values
+// dnc for when the values in the indicated cell must not contain the provided values.
+// The return is either (true, "") if the test passed, or (false, error_message) if the test failed.
+// The expected use is:
+// res, msg = CandTester(...)
+//
+//	if !res {
+//	    t.Fatalf(msg)
+//	}
 func CandTester(board [][]uint8, strat solver.Strategy, expected bool,
 	equal map[Cell][]uint8, contains map[Cell][]uint8, dnc map[Cell][]uint8) (bool, string) {
 	sol := solver.MakeSolver(board, []solver.Strategy{})
